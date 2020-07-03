3f5d28f495b69715a7814a4a8b62829b7dec642c
Владимир Шарапов <war-beast@yandex.ru>
2020-07-03 15:03:30 +0300

    Реализация паттерна Unit of work

---
+++ PlumsailTest.DAL/Interfaces/IUnitOfWorks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PlumsailTest.DAL.Entities;
+
+namespace PlumsailTest.DAL.Interfaces
+{
+	public interface IUnitOfWorks
+	{
+		IRepository<Submission> Submission { get; }
+
+		void Save();
+	}
+}
---
+++ PlumsailTest.DAL/Repositories/UnitOfWorks.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlumsailTest.DAL.Entities;
+using PlumsailTest.DAL.Interfaces;
+using System;
+
+namespace PlumsailTest.DAL.Repositories
+{
+	public class UnitOfWorks : IUnitOfWorks, IDisposable
+	{
+		#region private members
+
+		private ApplicationDataContext _db;
+		private SubmissionsRepository _submissionsRepository;
+
+		private bool disposed = false;
+
+		#endregion
+
+		#region constructor
+
+		public UnitOfWorks(DbContextOptions<ApplicationDataContext> options)
+		{
+			if (options == null)
+				throw new ArgumentNullException(nameof(options));
+
+			_db = new ApplicationDataContext(options);
+		}
+
+		#endregion
+
+		public IRepository<Submission> Submission => _submissionsRepository ??= new SubmissionsRepository(_db);
+
+		public void Save()
+		{
+			_db.SaveChanges();
+		}
+
+		public virtual void Dispose(bool disposing)
+		{
+			if (disposed) 
+				return;
+
+			if (disposing)
+			{
+				_db.Dispose();
+			}
+			this.disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
